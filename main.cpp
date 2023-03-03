6fe46f5ea5f2fd99ab72247ee640077962147bb5
RadchenkoMukola <113232389+RadchenkoMukola@users.noreply.github.com>
2023-03-03 14:03:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Figure.h"
+#include "UI.h"
+#include "Figurefunctions.h"
+#include <locale>
+#include <Windows.h> 
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    start();
+
+    return 0;
+}
